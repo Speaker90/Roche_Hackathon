14e1e71f748ca7effa2e8c25fdc924f98321e883
Florian Spychiger <spychiger88@gmail.com>
2018-11-22 14:49:27 +0100

    folder for working CNN

---
+++ CNN/train_CNN.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.optimizers import SGD
+from sklearn.model_selection import train_test_split
